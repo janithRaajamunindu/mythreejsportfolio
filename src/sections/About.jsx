a7216dd22b2e45ffa79d37d6542ec5ab0bb369de
Janith Raajamunindu <it22234384@my.sliit.lk>
2025-03-13 22:57:00 +0530

    fix error: about.jsx contact me

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -84,7 +84,13 @@ const About = () => {
               <p className="grid-subtext">
                 I'm based in Sri Lanka, and open to remote work worldwide.
               </p>
-              <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+              <a href="#contact">
+                <Button
+                  name="Contact Me"
+                  isBeam
+                  containerClass="w-full mt-10"
+                />
+              </a>
             </div>
           </div>
         </div>
